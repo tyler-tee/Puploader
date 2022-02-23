48cb1a9adf769cb30b09dfb9c7852e5ab74d7d0d
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2022-02-23 11:59:54 -0500

    +Minor protection against forced browsing

--- app/main.py
+++ app/main.py
@@ -81,56 +81,68 @@ def authenticated():
 
 @app.route('/upload')
 def puploader_upload():
-    folders = [folder for folder in os.listdir(app.config['UPLOAD_FOLDER']) if os.path.isdir(os.path.join(app.config['UPLOAD_FOLDER'], folder))]
-    folders.sort()
-    return render_template('upload.html', folders=folders)
+    if "username" in session:
+        folders = [folder for folder in os.listdir(app.config['UPLOAD_FOLDER']) if os.path.isdir(os.path.join(app.config['UPLOAD_FOLDER'], folder))]
+        folders.sort()
+        return render_template('upload.html', folders=folders)
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/uploaded', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST': # Check for appropriate method
-        # Proceed if there are files to upload - Else notify user.
-        files = request.files.getlist('files')
-        print(files)
-        if request.form['folder_dropdown'].lower() != 'default':
-            upload_folder = os.path.join(app.config['UPLOAD_FOLDER'], request.form['folder_dropdown'])
-        else:
-            upload_folder = app.config['UPLOAD_FOLDER']
-        
-        for file in files:
-            extension = file.filename.split('.')[-1].lower()
+    if "username" in session:
+        if request.method == 'POST': # Check for appropriate method
+            # Proceed if there are files to upload - Else notify user.
+            files = request.files.getlist('files')
+            print(files)
+            if request.form['folder_dropdown'].lower() != 'default':
+                upload_folder = os.path.join(app.config['UPLOAD_FOLDER'], request.form['folder_dropdown'])
+            else:
+                upload_folder = app.config['UPLOAD_FOLDER']
             
-            if extension in ('gif', 'jpg', 'jpeg', 'png'): # Check for allowed extensions
-                # If filename is a duplicate, tag w/ _dupe
-                while file.filename in os.listdir(upload_folder):
-                    file.filename = '.'.join(file.filename.split('.')[:-1]) + '_dupe.' + extension
-                file.save(os.path.join(upload_folder, secure_filename(file.filename)))
-            
-        flash('File(s) uploaded successfully!', 'success')
+            for file in files:
+                extension = file.filename.split('.')[-1].lower()
+                
+                if extension in ('gif', 'jpg', 'jpeg', 'png'): # Check for allowed extensions
+                    # If filename is a duplicate, tag w/ _dupe
+                    while file.filename in os.listdir(upload_folder):
+                        file.filename = '.'.join(file.filename.split('.')[:-1]) + '_dupe.' + extension
+                    file.save(os.path.join(upload_folder, secure_filename(file.filename)))
+                
+            flash('File(s) uploaded successfully!', 'success')
 
-        return redirect('/upload')
-            
+            return redirect('/upload')
+                
+        else:
+            return 'Something went wrong - Please try again.'
     else:
-        return 'Something went wrong - Please try again.'
+        return redirect(url_for('login'))
 
 
 @app.route('/gallery', methods=['GET'])
 def render_gallery():
-    folders = [folder for folder in os.listdir(app.config['UPLOAD_FOLDER']) if os.path.isdir(os.path.join(app.config['UPLOAD_FOLDER'], folder))]
-    folders.sort()
-    photos = [photo for photo in os.listdir(config['upload_folder']) if '.' in photo]
-    
-    return render_template('gallery.html', photos=photos, folders=folders)
+    if "username" in session:
+        folders = [folder for folder in os.listdir(app.config['UPLOAD_FOLDER']) if os.path.isdir(os.path.join(app.config['UPLOAD_FOLDER'], folder))]
+        folders.sort()
+        photos = [photo for photo in os.listdir(config['upload_folder']) if '.' in photo]
+        
+        return render_template('gallery.html', photos=photos, folders=folders)
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/gallery/<subfolder>', methods=['GET'])
 def render_subfolder_gallery(subfolder):
-    subfolder = os.path.join(app.config['UPLOAD_FOLDER'], subfolder)
-    folders = [folder for folder in os.listdir(app.config['UPLOAD_FOLDER']) if os.path.isdir(os.path.join(subfolder, folder))]
-    folders.sort()
-    photos = [photo for photo in os.listdir(subfolder) if '.' in photo]
+    if "username" in session:
+        subfolder = os.path.join(app.config['UPLOAD_FOLDER'], subfolder)
+        folders = [folder for folder in os.listdir(app.config['UPLOAD_FOLDER']) if os.path.isdir(os.path.join(subfolder, folder))]
+        folders.sort()
+        photos = [photo for photo in os.listdir(subfolder) if '.' in photo]
 
-    return render_template('gallery.html', photos=photos, folders=folders)
+        return render_template('gallery.html', photos=photos, folders=folders)
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/logout', methods=['POST', 'GET'])
