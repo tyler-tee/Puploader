7fbb3bf67b491060c0876fe05ff1b4a35e2e4487
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2022-02-27 11:24:57 -0500

    +App factory; Major refactor, +config rework

--- app/config.py
+++
@@ -1,5 +0,0 @@
-config = {
-    "private": False, # Changing this to False will disable certain functions of the app (IE, create_new_folder),
-    "upload_folder_max": 50,
-    "upload_folder": "./static/uploads"
-}
