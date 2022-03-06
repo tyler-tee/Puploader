b84bf4e91e5222dd78f067447fad841e5c16b028
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2022-03-05 20:52:10 -0500

    Minor formatting

--- app/app.py
+++ app/app.py
@@ -6,22 +6,22 @@ from petfinder_api.petfinder_api import PetFinder
 
 def create_app():
     app = Flask(__name__)
-    
+
     try:
         app.config.from_pyfile('./config/development.cfg')
     except FileNotFoundError:
         app.config.from_pyfile('./config/production.cfg')
-        
+    
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') if os.environ.get('SECRET_KEY') else app.config['SECRET_KEY']
 
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.mkdir(app.config['UPLOAD_FOLDER'])
-    
+
     mongo_uri = app.config['MONGODB_URI'] if app.config['MONGODB_URI'] else os.environ.get('MONGODB_URI')
     client = pymongo.MongoClient(mongo_uri, 27017)
     database = client['AUTH']
     users = database['USERS']
-    
+
     if os.environ.get('PETFINDER_KEY') and os.environ.get('PETFINDER_SEC'):
         petfinder_api = PetFinder(os.environ.get('PETFINDER_KEY'),
                                   os.environ.get('PETFINDER_SEC'))
