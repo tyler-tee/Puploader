2a7f842d1d6bd2dc8fae227ef997f8055b3c9e11
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2022-03-06 21:24:13 -0500

    +charitynav_api

---
+++ app/charitynav_api/charitynav_api.py
@@ -0,0 +1,35 @@
+import requests
+from typing import List
+
+
+class CharityNav_API:
+    """
+    Wrapper for CharityNavigator's REST API.
+    """
+
+    def __init__(self, app_id: str, app_key: str):
+        """
+        Constructor for CharityNav's wrapper.
+        """
+        self.app_id = app_id
+        self.app_key = app_key
+        self.base_url = "https://api.data.charitynavigator.org/v2"
+
+        self.client = requests.session()
+
+    def get_organizations(self, category_id: int, **kwargs) -> List:
+        """
+        Retrieve organizations based on category and any other supplied args.
+        """
+        params = {
+                "app_id": self.app_id,
+                "app_key": self.app_key,
+                "categoryID": category_id
+                }
+
+        orgs = self.client.get(f"{self.base_url}/organizations", params=params)
+
+        if orgs.status_code == 200:
+            return orgs.json()
+
+        print(orgs.status_code, orgs.headers, orgs.text)
